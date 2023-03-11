b9b605ee128e2f25ec2acd63bb64c7cd07fb4cb2
safecorners <safeconers.mobile@gmail.com>
2023-03-11 16:07:11 +0900

    add injector example

---
+++ example/injector/injection_example.py
@@ -0,0 +1,43 @@
+from typing import NewType
+
+from injector import Binder, Injector, Module, inject, provider
+
+Name = NewType("Name", str)
+Description = NewType("Description", str)
+
+
+class User:
+    @inject
+    def __init__(self, name: Name, description: Description) -> None:
+        self.name = name
+        self.description = description
+
+
+class UserModule(Module):
+    def configure(self, binder: Binder) -> None:
+        binder.bind(User)
+
+
+class UserAttributeModule(Module):
+    def configure(self, binder: Binder) -> None:
+        binder.bind(Name, to="Sherlock")
+
+    @provider
+    def describe(self, name: Name) -> Description:
+        return "%s is a man of astounding insight" % name
+
+
+class NumberModule(Module):
+    def configure(self, binder: Binder) -> None:
+        binder.bind(int, to=654)
+
+    @provider
+    def provider_str(self, i: int) -> str:
+        return str(i)
+
+
+injector = Injector([UserModule(), UserAttributeModule()])
+
+user = injector.get(User)
+print(f"{injector.get(Name)} - {injector.get(Description)}")
+print(f"{user.name} - {user.description}")
