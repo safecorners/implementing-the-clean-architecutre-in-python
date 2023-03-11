43d0940677906a3c443a8f16342bb921cd2ba8ac
safecorners <safeconers.mobile@gmail.com>
2023-03-11 21:22:05 +0900

    parse Request to Dto

---
+++ src/web_app/web_app/serialization/dto.py
@@ -0,0 +1,23 @@
+from typing import Type, TypeVar, cast
+
+from flask import Request, abort, jsonify, make_response
+from marshmallow import Schema, exceptions
+from marshmallow_dataclass import class_schema
+
+from auctions.domain.value_objects import Money
+from web_app.serialization.fields import Dollars
+
+TDto = TypeVar("TDto")
+
+
+class BaseSchema(Schema):
+    TYPE_MAPPING = {Money: Dollars}
+
+
+def get_dto(request: Request, dto_cls: Type[TDto], context: dict) -> TDto:
+    schema_cls = class_schema(dto_cls, base_schema=BaseSchema)
+    schema = schema_cls()
+    try:
+        return cast(TDto, schema.load(dict(context, **request.json)))
+    except exceptions.ValidationError as exc:
+        abort(make_response(jsonify(exc.messages), 400))
