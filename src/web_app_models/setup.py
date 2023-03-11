6e626a6c4f434c30fbfa01fa938fefed1353b802
safecorners <safeconers.mobile@gmail.com>
2023-03-11 18:07:55 +0900

    add web_app_model module for authenticating

---
+++ src/web_app_models/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="web_app_models",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["flask-security", "sqlalchemy", "db_infrastructure"],
+    extra_require={"dev": ["pytest"]},
+)
