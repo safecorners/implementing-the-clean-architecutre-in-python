cb81d3357de9a6cb7c0874ac37e20e8fdf90a2c6
safecorners <safeconers.mobile@gmail.com>
2023-03-14 04:41:07 +0900

    initialize payments module

---
+++ src/payments/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="payments",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "injector",
+        "sqlalchemy",
+        "stripe",
+        "requests",
+        "db_infrastructure",
+    ],
+    extras_require={"dev": ["pytest"]},
+)
