4e46ae938e8eb29b6f433ed12177aa79c0dce08d
safecorners <safeconers.mobile@gmail.com>
2023-03-16 18:25:34 +0900

    add Auction Events

---
+++ src/auctions/auctions/domain/events.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from auctions.domain.value_objects import AuctionId, BidderId
+from foundation.events import Event
+from foundation.value_objects import Money
+
+
+@dataclass(frozen=True)
+class BidderHasBeenOverbid(Event):
+    auction_id: AuctionId
+    bidder_id: BidderId
+    new_price: Money
+    auction_title: str
+
+
+@dataclass(frozen=True)
+class WinningBidPlaced(Event):
+    auction_id: AuctionId
+    winner_id: Optional[BidderId]
+    winning_bid: Money
+    auction_title: str
+
+
+@dataclass(frozen=True)
+class AuctionBegan(Event):
+    auction_id: AuctionId
+    starting_price: Money
+    auction_title: str
