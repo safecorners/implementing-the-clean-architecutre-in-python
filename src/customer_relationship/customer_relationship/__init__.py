a2f1edb70e89c6be9f241ffe840cc26343ec1e85
safecorners <safeconers.mobile@gmail.com>
2023-03-17 23:18:21 +0900

    register event handler

--- src/customer_relationship/customer_relationship/__init__.py
+++ src/customer_relationship/customer_relationship/__init__.py
@@ -1,9 +1,11 @@
 import injector
 from sqlalchemy.engine import Connection
 
+from auctions import BidderHasBeenOverbid, WinningBidPlaced
 from customer_relationship.config import CustomerRelationshipConfig
 from customer_relationship.facade import CustomerRelationshipFacade
 from customer_relationship.models import customers
+from foundation.events import AsyncEventHandlerProvider, AsyncHandler
 
 __all__ = [
     # module
@@ -22,3 +24,35 @@ class CustomerRelationship(injector.Module):
         self, config: CustomerRelationshipConfig, connection: Connection
     ) -> CustomerRelationshipFacade:
         return CustomerRelationshipFacade(config, connection)
+
+    def configure(self, binder: injector.Binder) -> None:
+        binder.multibind(
+            AsyncHandler[BidderHasBeenOverbid],
+            to=AsyncEventHandlerProvider(BidderHasBeenOverbidHandler),
+        )
+        binder.multibind(
+            AsyncHandler[WinningBidPlaced],
+            to=AsyncEventHandlerProvider(WinningBidPlacedHandler),
+        )
+
+
+class BidderHasBeenOverbidHandler:
+    @injector.inject
+    def __init__(self, facade: CustomerRelationshipFacade) -> None:
+        self._facade = facade
+
+    def __call__(self, event: BidderHasBeenOverbid) -> None:
+        self._facade.send_email_about_overbid(
+            event.bidder_id, event.new_price, event.auction_title
+        )
+
+
+class WinningBidPlacedHandler:
+    @injector.inject
+    def __init__(self, facade: CustomerRelationshipFacade) -> None:
+        self._facade = facade
+
+    def __call__(self, event: WinningBidPlaced) -> None:
+        self._facade.send_email_about_winning(
+            event.bidder_id, event.bid_amount, event.auction_title
+        )
--- src/payments/payments/__init__.py
+++ src/payments/payments/__init__.py
@@ -1,7 +1,7 @@
 import injector
 from sqlalchemy.engine import Connection
 
-from foundation.events import EventBus
+from foundation.events import AsyncEventHandlerProvider, AsyncHandler, EventBus
 from payments.config import PaymentsConfig
 from payments.events import (
     PaymentCaptured,
@@ -31,3 +31,18 @@ class Payments(injector.Module):
         self, config: PaymentsConfig, connection: Connection, event_bus: EventBus
     ) -> PaymentsFacade:
         return PaymentsFacade(config, connection, event_bus)
+
+    def configure(self, binder: injector.Binder) -> None:
+        binder.multibind(
+            AsyncHandler[PaymentCharged],
+            to=AsyncEventHandlerProvider(PaymentChargedHandler),
+        )
+
+
+class PaymentChargedHandler:
+    @injector.inject
+    def __init__(self, facade: PaymentsFacade) -> None:
+        self._facade = facade
+
+    def __call__(self, event: PaymentCharged) -> None:
+        self._facade.capture(event.payment_uuid, event.customer_id)
